184009bb3e082c5025776d38a429f443c36273cb
Samuel Hoyas <samuelix97@gmail.com>
2024-04-16 12:01:33 +0200

    Counters in canvas

--- src/utils/images.ts
+++ src/utils/images.ts
@@ -2,4 +2,17 @@ const uncheckedSectionImage = document.getElementById(
   'uncheckedSectionImage',
 ) as HTMLImageElement;
 
-export { uncheckedSectionImage };
+const imageNumbers = [
+  document.getElementById('zeroImage') as HTMLImageElement,
+  document.getElementById('oneImage') as HTMLImageElement,
+  document.getElementById('twoImage') as HTMLImageElement,
+  document.getElementById('threeImage') as HTMLImageElement,
+  document.getElementById('fourImage') as HTMLImageElement,
+  document.getElementById('fiveImage') as HTMLImageElement,
+  document.getElementById('sixImage') as HTMLImageElement,
+  document.getElementById('sevenImage') as HTMLImageElement,
+  document.getElementById('eightImage') as HTMLImageElement,
+  document.getElementById('nineImage') as HTMLImageElement,
+];
+
+export { uncheckedSectionImage, imageNumbers };
